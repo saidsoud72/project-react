bc364d858e3ce6bfc498787e2b591d4d4c078ae9
saidsoud72 <said.omar@student.moringaschool.com>
2022-10-12 12:05:36 +0300

    update js test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
-import Home from './Home'
-import {Route, Switch} from 'react-router-dom'
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Feedback from './Feedback';
-import Book from './Book';
-import Subscribe from './Subscribe';
-import Offers from './Offers';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "./Login";
+import Notes from "./Notes";
+import Home from "./Home";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [newData, setNewData] = useState(false);
+  const [search, setSearch] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    fetch("http://localhost:3000/notes")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setNotes(data);
+      });
+  }, [newData]);
+
+  function handleNewData() {
+    setNewData((newData) => !newData);
+  }
+  function handleDelete(id) {
+    fetch(`http://localhost:3000/notes/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        const updatedNotes = notes.filter(
+          (remainingNotes) => remainingNotes.id !== id
+        );
+        setNotes(updatedNotes);
+      });
+  }
   return (
-      <>
-      <Navbar/>
+    <div className="App">
+      <NavBar setIsLoggedIn={setIsLoggedIn} setSearch={setSearch} />
+      <hr />
       <Switch>
-        <Route exact path="/feedback">
-          <Feedback/>
+        <Route exact path="/notes">
+          <Notes isLoggedIn={isLoggedIn} handleNewData={handleNewData} />
         </Route>
-        <Route exact path="/book">
-          <Book/>
-        </Route>
-        <Route exact path="/contact">
-          <Subscribe/>
-        </Route>
-        <Route exact path="/offers">
-          <Offers/>
+        <Route exact path="/login">
+          <Login setIsLoggedIn={setIsLoggedIn} />
         </Route>
         <Route exact path="/">
-          <Home/>
+          <Home
+            isLoggedIn={isLoggedIn}
+            notes={notes}
+            search={search}
+            handleDelete={handleDelete}
+          />
         </Route>
       </Switch>
-      <Footer/>
-      </>
+    </div>
   );
 }
 
